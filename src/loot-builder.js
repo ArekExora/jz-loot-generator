8c2e2e8239e17ff59e29be55061a5f29a2ac0320
Diego Rodríguez <ArekExora@users.noreply.github.com>
2023-03-26 01:50:47 +0100

    chore: added all gemstones

--- src/loot-builder.js
+++ src/loot-builder.js
@@ -36,23 +36,68 @@ export class LootBuilder {
   },{
     type: 'Gemstones',
     valueInGp: 50,
-    items: [ ]
+    items: [
+      { name: 'Bloodstone', description: 'Opaque dark gray with red flecks', img: 'icons/commodities/gems/gem-rough-rose-red.webp' },
+      { name: 'Carnelian', description: 'Opaque orange to red-brown', img: 'icons/commodities/gems/gem-rough-round-orange.webp' },
+      { name: 'Chaldedony', description: 'Opaque white', img: 'icons/commodities/gems/pearl-white-oval.webp' },
+      { name: 'Chrysoprase', description: 'Translucent green', img: 'icons/commodities/gems/gem-fragments-green.webp' },
+      { name: 'Citrine', description: 'Transparent pale yellow-brown', img: 'icons/commodities/gems/gem-rough-pear-orange.webp' },
+      { name: 'Jasper', description: 'Opaque blue, black or brown', img: 'icons/commodities/gems/gem-rough-cushion-purple.webp' },
+      { name: 'Moonstone', description: 'Translucent white with pale blue glow', img: 'icons/commodities/gems/gem-rough-drop-blue.webp' },
+      { name: 'Onyx', description: 'Opaque bands of black and white, or pure black or white', img: 'icons/commodities/gems/powder-raw-white.webp' },
+      { name: 'Quartz', description: 'Transparent white, smoky gray, or yellow', img: 'icons/commodities/gems/gem-rough-oval-white.webp' },
+      { name: 'Sardonyx', description: 'Opaque bands of red and white', img: 'icons/commodities/gems/gem-faceted-rough-red.webp' },
+      { name: 'Star rose quartz', description: 'Translucent rosy stone with white star-shaped center', img: 'icons/commodities/gems/gem-rough-round-pink.webp' },
+      { name: 'Zircon', description: 'Transparent pale blue-green', img: 'icons/commodities/gems/gem-fragments-turquoise.webp' },
+    ]
   },{
     type: 'Gemstones',
     valueInGp: 100,
-    items: [ ]
+    items: [
+      { name: 'Amber', description: 'Transparent watery gold to rich gold', img: 'icons/commodities/gems/gem-amber-insect-orange.webp' },
+      { name: 'Amethyst', description: 'Transparent deep purple', img: 'icons/commodities/gems/gem-cut-faceted-princess-purple.webp' },
+      { name: 'Chrysoberyl', description: 'Transparent yellow-green to pale green', img: 'icons/commodities/gems/gem-raw-rough-green-yellow.webp' },
+      { name: 'Coral', description: 'Opaque crimsom', img: 'icons/commodities/gems/gem-rough-cushion-red.webp' },
+      { name: 'Garnet', description: 'Transparent red, brown-green or violet', img: 'icons/commodities/gems/gem-rough-oval-purple.webp' },
+      { name: 'Jade', description: 'Translucent light green, deep green or white', img: 'icons/commodities/gems/gem-rough-oval-green.webp' },
+      { name: 'Jet', description: 'Opaque deep black', img: 'icons/commodities/gems/gem-rough-ball-purple.webp' },
+      { name: 'Pearl', description: 'Opaque lustrous white, yellow or pink', img: 'icons/commodities/gems/pearl-rough-white.webp' },
+      { name: 'Spinel', description: 'Transparent red, red-brown or deep green', img: 'icons/commodities/gems/gem-rough-square-red.webp' },
+      { name: 'Tourmaline', description: 'Transparent pale green, blue, brown or red', img: 'icons/commodities/gems/pearl-rough-turquoise.webp' },
+    ]
   },{
     type: 'Gemstones',
     valueInGp: 500,
-    items: [ ]
+    items: [
+      { name: 'Alexandrite', description: 'Transparent dark green', img: 'icons/commodities/gems/gem-faceted-large-green.webp' },
+      { name: 'Aquamarine', description: 'Transparent pale blue-green', img: 'icons/commodities/gems/gem-shattered-teal.webp' },
+      { name: 'Black pearl', description: 'Opaque pure black', img: 'icons/commodities/gems/pearl-glass-purple.webp' },
+      { name: 'Blue spinel', description: 'Transparent deep blue', img: 'icons/commodities/gems/gem-shattered-violet.webp' },
+      { name: 'Peridot', description: 'Transparent rich olive green', img: 'icons/commodities/gems/gem-rough-brilliant-green.webp' },
+      { name: 'Topaz', description: 'Transparent golden yellow', img: 'icons/commodities/gems/gem-rough-rectangular-red.webp' },
+    ]
   },{
     type: 'Gemstones',
     valueInGp: 1000,
-    items: [ ]
+    items: [
+      { name: 'Black opal', description: 'Translucent dark green with black mottling and golden flecks', img: 'icons/commodities/gems/gem-faceted-diamond-green.webp' },
+      { name: 'Blue sapphire', description: 'Transparent blue-white to medium blue', img: 'icons/commodities/gems/gem-faceted-radiant-blue.webp' },
+      { name: 'Emerald', description: 'Transparent deep bright green', img: 'icons/commodities/gems/gem-cut-table-green.webp' },
+      { name: 'Fire opal', description: 'Translucent fiery red', img: 'icons/commodities/gems/gem-oval-red.webp' },
+      { name: 'Opal', description: 'Translucent pale blue with green and golden mottling', img: 'icons/commodities/gems/gem-faceted-hexagon-blue.webp' },
+      { name: 'Star ruby', description: 'Translucent ruby with white star-shaped center', img: 'icons/commodities/gems/pearl-fire-swirl.webp' },
+      { name: 'Star sapphire', description: 'Translucent blue sapphire with white star-shaped center', img: 'icons/commodities/gems/pearl-swirl-blue.webp' },
+      { name: 'Yellow sapphire', description: 'Transparent fiery yellow or yellow-green', img: 'icons/commodities/gems/gem-faceted-octagon-yellow.webp' },
+    ]
   },{
     type: 'Gemstones',
     valueInGp: 5000,
-    items: [ ]
+    items: [
+      { name: 'Black sapphire', description: 'Translucent lustrous black with glowing highlights', img: 'icons/commodities/gems/gem-faceted-round-black.webp' },
+      { name: 'Diamond', description: 'Transparent blue-white, canary, pink, brown or blue', img: 'icons/commodities/gems/gem-faceted-round-white.webp' },
+      { name: 'Jacinth', description: 'Transparent fiery orange', img: 'icons/commodities/gems/gem-faceted-trillion-orange.webp' },
+      { name: 'Ruby', description: 'Transparent clear red to deep crimsom', img: 'icons/commodities/gems/gem-faceted-radiant-red.webp' },
+    ]
   },{
     type: 'ArtObjects',
     valueInGp: 25,
